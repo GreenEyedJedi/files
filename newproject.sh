38a78c1ff762973c97640f19ca08a50dbaebfc15
Alec Brownlie <brownlia@seawolf.cs.sonoma.edu>
2013-10-24 18:00:56 -0700

    another commit

--- newproject.sh
+++ newproject.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # First project script
 
-echo "Hello World!"
+echo "Hey $USER! What type of project do you want to generate?"
+echo "1) Web"
+echo "2) CPP"
+echo ""
+echo "Please enter a number"
+read NUM 
+if [ $NUM = 1 ]; then 
+    echo "Okay, lets make a Web template."
+    
+
+
+
