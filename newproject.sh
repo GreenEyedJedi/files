f9651208c1257352d812cde5b9ef05ce075a9000
GreenEyedJedi <alecbrownlie@gmail.com>
2013-10-31 13:47:04 -0700

    Update newproject.sh

--- newproject.sh
+++ newproject.sh
@@ -6,10 +6,12 @@ echo "1) Web"
 echo "2) CPP"
 echo ""
 echo "Please enter a number"
-read NUM 
+read $NUM 
 if [ $NUM = 1 ]; then 
     echo "Okay, lets make a Web template."
-    
+    echo "What would you like to name your file as?"
+    read $filename
+    touch $filename
 
 
 
